5022d90a67d9210e5471628251cedb2df0140951
Aidan Mascoli <145822273+denayeisnotokay@users.noreply.github.com>
2024-10-23 20:56:54 -0400

    remove node_modules (run npm install locally, library files should be ignored), added .gitignore, and fixed index route

--- app.js
+++ app.js
@@ -16,6 +16,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/', (req, res) => {
+  res.render('index');
+})
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
